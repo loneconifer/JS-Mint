09667d00b3a514695719167c3c515d94c3f08a64
Banajyoti Nath <145108158+loneconifer@users.noreply.github.com>
2024-12-28 16:56:46 +0000

    Basics of data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; // Treat all JavaScript code as newer version
+
+// alert(2 + 2) // We are using node js, not browser
+
+/*
+Documentation of alert method
+https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+*/
+
+console.log(2 
+    +
+     2); // Code readability should be high
+
+console.log("Conifer");
+
+// https://tc39.es/ecma262/#sec-intro // Official documentation
+
+let name = "Nibir"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number - 2 to power 53
+// bigInt
+// String - ""
+// Boolean - true or false
+// null - standalone value
+// undefined
+// symbol - unique
+
+
+// object
+
+console.log(typeof "conifer");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
