57b5bfcd64b1975b7dc993599f96594e23e33354
Banajyoti Nath <145108158+loneconifer@users.noreply.github.com>
2025-01-03 18:10:54 +0000

    Conversion of data types done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = 32
+// let score = "32"
+// let score = "32abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "conifer"
+
+// console.log(typeof score);
+// console.log(typeof(score)); // As method
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "32" = 32
+// "32abc" = NaN
+// true = 1; false = 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "juniper"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// "" = false
+// "juniper" = true
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+// console.log(typeof stringNumber);
