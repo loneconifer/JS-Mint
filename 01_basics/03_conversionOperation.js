e9adc524d30fce820a65b406ec0fe3a3b312bbcf
Banajyoti Nath <145108158+loneconifer@users.noreply.github.com>
2025-01-04 14:11:41 +0000

    Started about operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,5 +33,30 @@ let someNumber = 23
 
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
-
 // console.log(typeof stringNumber);
+
+
+// ****************** Operations ******************
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Hello"
+let str2 = " Conifer"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
