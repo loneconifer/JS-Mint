b1604de96357026a05c28acad2dca3493a52849d
Banajyoti Nath <145108158+loneconifer@users.noreply.github.com>
2025-01-07 05:56:37 +0000

    Data type comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(3 > 2);
+// console.log(3 >= 2);
+// console.log(3 < 2);
+// console.log(3 == 2);
+// console.log(3 != 2);
+
+// console.log("3" > 2);
+// console.log("03" > 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// Strict check ===
+
+// console.log("3" === 3);
