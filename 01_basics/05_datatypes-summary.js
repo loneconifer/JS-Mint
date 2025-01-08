a44cb31a153401a8ae95b44d035fbc78c1194387
Banajyoti Nath <145108158+loneconifer@users.noreply.github.com>
2025-01-08 16:37:30 +0000

    Data type summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,40 @@
+// // Primitive
+
+// // Seven types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3566645825456424n // BigInt
+
+
+// // Reference (Non primitive)
+
+// // Array, Objects, Functions
+
+// const heros = ["Delta", "Recon", "SEAL"]
+
+// let myObj = {
+//     name: "Conifer",
+//     age: 24,
+// }
+
+// const myFunction = function () {
+//     console.log("Hello World!");
+// }
+
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof myFunction);
+// console.log(typeof anotherId);
+
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
